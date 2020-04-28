bfc1698bf481960d31fec3e92bd447c434089826
YONGXI YANG <reganyang0415@gamil.com>
2020-04-28 14:20:34 +0800

    docker file hvd-py376-tf21-cpu

--- envcmd.sh
+++ envcmd.sh
@@ -1,19 +1,17 @@
-FROM ubuntu:18.04
-
+set -ex
 # TensorFlow version is tightly coupled to CUDA and cuDNN so it should be selected carefully
-ENV TENSORFLOW_VERSION=2.1.0
-SHELL ["/bin/bash", "-cu"]
+TENSORFLOW_VERSION=2.1.0
 
 # RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends apt-utils
-ENV DEBIAN_FRONTEND=noninteractive
-RUN apt-get update && apt-get install --assume-yes --no-install-recommends apt-utils
+DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install --assume-yes --no-install-recommends apt-utils
 
-RUN apt-get install -y bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev \
+apt-get install -y bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev \
     && apt-get install -y build-essential libsqlite3-dev sqlite3 \
     && apt-get install -y libgdbm-compat-dev liblzma-dev libreadline-dev \
     && apt-get install -y libncursesw5-dev libffi-dev uuid-dev
 
-RUN apt-get update && apt-get install -y --allow-downgrades --allow-change-held-packages --no-install-recommends \
+apt-get update && apt-get install -y --allow-downgrades --allow-change-held-packages --no-install-recommends \
         cmake \
         g++-4.8 \
         git \
@@ -28,11 +26,11 @@ RUN apt-get update && apt-get install -y --allow-downgrades --allow-change-held-
         libibverbs1 \
         ibverbs-providers
 
-RUN rm -rf /var/lib/apt/lists/*
+rm -rf /var/lib/apt/lists/*
 # RUN apt-get install -y wget gcc g++ && rm -rf /var/lib/apt/lists/*
 
 # && apt-get install openssl-devel bzip2-devel expat-devel gdbm-devel readline-devel sqlite-deve -y \
-RUN mkdir /tmp/openmpi && \
+mkdir /tmp/openmpi && \
     cd /tmp/openmpi && \
     wget https://www.open-mpi.org/software/ompi/v4.0/downloads/openmpi-4.0.0.tar.gz && \
     tar zxf openmpi-4.0.0.tar.gz && \
@@ -44,7 +42,7 @@ RUN mkdir /tmp/openmpi && \
     rm -rf /tmp/openmpi
 
 #python3.7.6 pls compile with --with-ssl or you can not use pip3
-RUN cd /tmp \
+cd /tmp \
     && tar -zvxf Python-3.7.6.tgz \
     && wget https://www.python.org/ftp/python/3.7.6/Python-3.7.6.tgz \
     && cd Python-3.7.6 \
@@ -66,28 +64,26 @@ RUN cd /tmp \
 #     ln -sf /usr/local/bin/python3 /usr/bin/python
  
 #python3-pip
-RUN pip install --upgrade pip
+pip install --upgrade pip
  
 #numpy setup
-RUN pip install --no-cache-dir tensorflow-cpu sklearn scipy pandas numpy
+pip install --no-cache-dir tensorflow sklearn scipy pandas numpy
 
 #horovod setup
-RUN HOROVOD_WITH_TENSORFLOW=1 \
+HOROVOD_WITH_TENSORFLOW=1 \
     pip install --no-cache-dir horovod[tensorflow,keras]
 
 # Install OpenSSH for MPI to communicate between containers
-RUN apt update && \
-    apt-get install -y --no-install-recommends openssh-client openssh-server && \
+apt update && \
+apt-get install -y --no-install-recommends openssh-client openssh-server && \
     mkdir -p /var/run/sshd
  
 # Allow OpenSSH to talk to containers without asking for confirmation
-RUN cat /etc/ssh/ssh_config | grep -v StrictHostKeyChecking > /etc/ssh/ssh_config.new && \
+cat /etc/ssh/ssh_config | grep -v StrictHostKeyChecking > /etc/ssh/ssh_config.new && \
     echo "    StrictHostKeyChecking no" >> /etc/ssh/ssh_config.new && \
     mv /etc/ssh/ssh_config.new /etc/ssh/ssh_config
 
 # Download examples
-RUN apt-get install -y --no-install-recommends subversion && \
+apt-get install -y --no-install-recommends subversion && \
     svn checkout https://github.com/horovod/horovod/trunk/examples && \
     rm -rf /examples/.svn
-
-WORKDIR "/examples"
