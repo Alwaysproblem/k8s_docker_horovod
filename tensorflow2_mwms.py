2f4f0eb600e0ea63b6607cb72e4e9f9dc9c411d2
YONGXI YANG <reganyang0415@gamil.com>
2020-05-01 14:28:37 +0800

    test for multiworker

---
+++ tensorflow2_mwms.py
@@ -0,0 +1,59 @@
+#%%
+import json
+import os
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+tfds.disable_progress_bar()
+
+os.environ['TF_CONFIG'] = json.dumps({
+    'cluster': {
+        'worker': ["172.17.67.60:12345", "172.17.67.59:23456"]
+    },
+    'task': {'type': 'worker', 'index': 0}
+})
+
+strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
+
+#%%
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+
+def make_datasets_unbatched():
+  # Scaling MNIST data from (0, 255] to (0., 1.]
+  def scale(image, label):
+    image = tf.cast(image, tf.float32)
+    image /= 255
+    return image, label
+
+  datasets, info = tfds.load(name='mnist',
+                            with_info=True,
+                            as_supervised=True)
+
+  return datasets['train'].map(scale).cache().shuffle(BUFFER_SIZE)
+
+train_datasets = make_datasets_unbatched().batch(BATCH_SIZE).repeat(3)
+
+#%%
+def build_and_compile_cnn_model():
+  model = tf.keras.Sequential([
+      tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(28, 28, 1)),
+      tf.keras.layers.MaxPooling2D(),
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(64, activation='relu'),
+      tf.keras.layers.Dense(10)
+  ])
+  model.compile(
+      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+      optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
+      metrics=['accuracy'])
+  return model
+
+#%%
+with strategy.scope():
+    model = build_and_compile_cnn_model()
+
+model.fit(x=train_datasets, epochs=3, steps_per_epoch=5)
+
+# %%
