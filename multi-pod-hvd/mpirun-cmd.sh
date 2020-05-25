9579d2d792bc89d96992ca913120f99f27a81401
Alwaysproblem <reganyang0415@gmailc.com>
2020-05-25 11:36:29 +0000

    clean

--- multi-pod-hvd/mpirun-cmd.sh
+++
@@ -1,6 +0,0 @@
-mpirun \
--np 3 \
--H n-adx-recall-2:1,n-adx-recall-3:1,n-adx-recall-4:1 \
---allow-run-as-root -bind-to none \
--map-by slot -x LD_LIBRARY_PATH \
--x PATH -mca pml ob1 -mca btl ^openib python /examples/tensorflow_mnist.py
