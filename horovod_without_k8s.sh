c223afa1ece90793697e1b66356c44bb7100be88
YONGXI YANG <reganyang0415@gamil.com>
2020-06-17 16:40:03 +0800

    init

--- horovod_without_k8s.sh
+++
@@ -1,3 +0,0 @@
-docker run --privileged --rm -it --name hvd --network=host -v "/home/sdev/share/ssh:/root/.ssh/" alwaysproblem/adalgohvd /bin/bash -c "/usr/sbin/sshd -p 23445; sleep infinity"
-docker run --privileged --rm -it --name hvd --network=host -v "/data02/hvd_share/ssh:/root/.ssh/" alwaysproblem/adalgohvd /bin/bash -c "/usr/sbin/sshd -p 23445; sleep infinity"
-horovodrun --start-timeout 600 -np 16 -H n-adx-recall-2:8,n-adx-recall-3:8 -p 23445 python fashion.py
